0e24fcadc1d5b0e0f1909f955902911d1a41497f
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:03:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,18 @@
 class Book
   
+  attr_accessor = :title, :author
+  
   def initialize(title)
     @title = title
   end
   
-  def title
-    @title
-  end
+  # def title
+  #   @title
+  # end
   
-  def author=(author)
-    @author = author
-  end
+  # def author=(author)
+  #   @author = author
+  # end
   
 end
 
