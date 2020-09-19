c80612edc06ca7beb1329397eb794bb2124803ad
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:05:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   
   attr_accessor = :title, :author, :page_count
   
-  def initialize(title)
+  def initialize(title, author, page_count)
     @title = title
     @author = author
     @page_count = page_count
