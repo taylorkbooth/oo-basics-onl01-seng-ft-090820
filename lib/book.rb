f9720df17ff550af060565e708e8d9e12975dceb
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:07:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,19 @@ class Book
     @author = author
   end
   
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+    
+  
 end
 
 Book.new("And Then There Were None")
