2fe8b749bf36d1b99a75caee1e9bfdf82d1f6981
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:10:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   
-  # attr_accessor = :title, :author, :page_count
+  attr_accessor = :title, :author, :page_count
   
   def initialize(title)
     @title = title
@@ -8,37 +8,37 @@ class Book
     # @page_count = page_count
   end
   
-  def title
-    @title
-  end
+  # def title
+  #   @title
+  # end
   
-  def author=(author)
-    @author = author
-  end
+  # def author=(author)
+  #   @author = author
+  # end
   
-  def author
-    @author
-  end
+  # def author
+  #   @author
+  # end
   
-  def page_count=(num)
-    @page_count = num
-  end
+  # def page_count=(num)
+  #   @page_count = num
+  # end
   
-  def page_count
-    @page_count
-  end
+  # def page_count
+  #   @page_count
+  # end
   
-  def genre=(genre)
-    @genre = genre
-  end
+  # def genre=(genre)
+  #   @genre = genre
+  # end
   
-  def genre
-    @genre
-  end
+  # def genre
+  #   @genre
+  # end
   
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
     
   
 end
