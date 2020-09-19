bcd093fe64cddb5e42704c291f6fb1d7ce4ce35d
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:21:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -17,3 +17,20 @@
 # end
 
 # Shoe.new(Nike)
+
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
+
+Shoe.new("Nike")
