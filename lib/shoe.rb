7717c3a0b4e69e38c53df59716af0b5c1cbb01a1
Taylor Booth <taylorkbooth@gmail.com>
2020-09-19 20:21:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,12 @@
 class Shoe
 
-attr_reader = :brand
-attr_accessor = :color, :size, :material, :condition
-  
   def initialize(brand)
     @brand= brand
   end
+  
+attr_reader = :brand
+attr_accessor = :color, :size, :material, :condition
+  
   
   def cobble()
     self.condition = "new"
